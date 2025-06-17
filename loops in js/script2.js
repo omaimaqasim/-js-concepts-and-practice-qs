b19225efb27c2242aabbcb04b8b052d23cc2be26
omaimaqasim <omaimaakhan15@gmail.com>
2025-06-17 21:52:46 +0500

    loops in js

---
+++ loops in js/script2.js
@@ -0,0 +1,42 @@
+console.log("LOOP PRACTICE QS");
+
+console.log("");
+console.log("qno 2");
+// write a program to print the marks of student in an object using for in  loop
+
+const student_marks={
+        omaima:89,
+        sakina:90,
+       sadia:99,
+    //   "" ye zaroori hai aghar variable string
+
+        
+}
+
+for (const key in student_marks) {
+   
+        const element = student_marks[key];
+        console.log(key,element)
+        
+  
+}
+
+console.log("");
+console.log("qno 2");
+// 
+
+
+
+
+console.log("");
+console.log("qno 3");
+// write a program to print try again until the user enter the 
+// correct number
+
+ let no="rubi";
+
+ 
+
+
+
+
